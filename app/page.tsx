d0167f3e8eae84c479e0739b9ddfa8f22fb8900d
527coco <527coco@naver.com>
2024-03-11 20:07:15 +0900

    chore : tanstack-query 세팅
    Refs:[#1]

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import {Spinner} from "@chakra-ui/spinner";
 import {Box} from "@chakra-ui/layout";
 
 export default function Home() {

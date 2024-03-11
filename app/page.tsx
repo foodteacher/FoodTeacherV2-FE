41bb781c375f1fd26ac67e787ef0c9adc3f5f5d5
527coco <527coco@naver.com>
2024-03-11 20:53:10 +0900

    chore : prettier&eslint 세팅
    Refs:[#1]

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+
+
 import {Box} from "@chakra-ui/layout";
-import {Button} from "@/app/shared/component";
+import {Button, KaKaoButtonIcon} from "@/app/(featured-slice)/shared/component";
 
 export default function Home() {
+
+
+    const redirect_uri =
+        process.env.NODE_ENV === "development"
+            ? "http://localhost:3000/oauth"
+            : "https://foodteacher.xyz/oauth";
+
+    // oauth 요청 URL
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_KEY}&redirect_uri=${redirect_uri}&response_type=code`;
+
+    const signUpKakaoHandler = () => {
+        window.location.href = kakaoURL;
+    };
+
     return (
         <main>
             <Box>
                 mainPage
             </Box>
-            <Button>123</Button>
+
+            <Box onClick={() => signUpKakaoHandler()}>
+                <KaKaoButtonIcon/>
+            </Box>
         </main>
     )
 }

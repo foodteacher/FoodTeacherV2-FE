39814b3f2607f5125fc5c7006c3978ef943bb00f
527coco <527coco@naver.com>
2024-03-11 20:21:43 +0900

    chore : FSD 아키텍쳐 초기 세팅
    Refs:[#4]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 import ChakraProviders from "@/app/ChakraProvider";
 import QueryProviders from "@/app/QueryProviders";
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import {Box} from "@chakra-ui/layout";
+import {Button} from "@/app/shared/component";
 
 export default function Home() {
-  return (
-    <main>
-      <Box>
-        mainPage
-      </Box>
-    </main>
-  )
+    return (
+        <main>
+            <Box>
+                mainPage
+            </Box>
+            <Button>123</Button>
+        </main>
+    )
 }

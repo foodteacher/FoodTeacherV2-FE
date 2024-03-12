3be99e994427f9a6b25a5abc57899866f28eee8f
527coco <527coco@naver.com>
2024-03-12 21:11:30 +0900

    feat(auth) : auth login form 컴포넌트
    Refs[#2]

--- app/(featured-slice)/features/auth/consts/consts.ts
+++ app/(featured-slice)/features/auth/consts/consts.ts
@@ -1,7 +1,7 @@
-export const KAKAO_URL = (redirect_uri: string) => {
+export const KAKAO_URL = (redirect_uri: string): string => {
   return `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_KEY}&redirect_uri=${redirect_uri}&response_type=code`;
 };
 
-export const NAVER_URL = (redirect_uri: string) => {
+export const NAVER_URL = (redirect_uri: string): string => {
   return `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_REST_ID}&state=${false}&redirect_uri=${redirect_uri}`;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import { Box } from "@chakra-ui/layout";
-import { Button, KaKaoButtonIcon } from "@/app/(featured-slice)/shared/UI";
-import { useSocialLogin } from "./(featured-slice)/features/auth/hooks";
+import { useSocialToken } from "./(featured-slice)/features/auth/hooks";
+import LoginForm from "./(featured-slice)/features/auth/ui/LoginForm";
 
 export default function Home() {
-  const { signUpKakaoHandler, signUpNaverHandler } = useSocialLogin();
   return (
     <main>
-      <Box>mainPage</Box>
-
-      <Box onClick={() => signUpKakaoHandler()}>
-        <KaKaoButtonIcon />
-      </Box>
-
-      <Button onClick={() => signUpNaverHandler()}>네이버 로그인</Button>
+      <LoginForm />
     </main>
   );
 }

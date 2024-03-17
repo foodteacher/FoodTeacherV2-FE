01866b80336a45b3151c8a6e8b77f0fa26eb1edd
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-03-17 18:53:10 +0900

    docs : 안쓴느 함수 제거 및 코드 포맷팅
    Refs:[#2]

--- app/(featured-slice)/features/auth/ui/LoginForm.tsx
+++ app/(featured-slice)/features/auth/ui/LoginForm.tsx
@@ -3,7 +3,6 @@ import { KaKaoButtonIcon } from "@/app/(featured-slice)/shared/UI";
 import { Box, Button, ButtonGroup } from "@chakra-ui/react";
 import React from "react";
 import { useSocialToken } from "../hooks";
-import { getData } from "@/app/(featured-slice)/shared/api/SharedApi";
 
 const LoginForm = () => {
   const { signUpKakaoHandler, signUpNaverHandler } = useSocialToken();
@@ -14,7 +13,6 @@ const LoginForm = () => {
         <KaKaoButtonIcon />
       </Box>
       <Button onClick={() => signUpNaverHandler()}>네이버 로그인</Button>
-      <Button onClick={() => getData()}>test</Button>
     </ButtonGroup>
   );
 };

9c5e93cdf0914bad6cff6b4be776333c4083690e
527coco <527coco@naver.com>
2024-03-18 20:07:12 +0900

    feat : react-speech-recognition 의존성 설치
    regenerator-runtime 패키지 추가 설치 및 hydration error 해결
    
    Ref:[#9]

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import dynamic from "next/dynamic";
 import LoginForm from "./(featured-slice)/features/auth/ui/LoginForm";
 import styles from "./page.module.css";
+const SttForm = dynamic(
+  () => import("./(featured-slice)/features/stt/ui/SttForm"),
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
     <main>
       <LoginForm />
+      <SttForm />
     </main>
   );
 }

f23376b4b5bddca98fb5be2039bd03fba17ff27e
527coco <527coco@naver.com>
2024-03-18 19:12:03 +0900

    feat : form 시멘틱 태그 추가
    Refs:[#2]

--- app/(featured-slice)/features/auth/ui/LoginForm.tsx
+++ app/(featured-slice)/features/auth/ui/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { KaKaoButtonIcon } from "@/app/(featured-slice)/shared/UI";
-import { Box, Button, ButtonGroup } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Flex } from "@chakra-ui/react";
 import React from "react";
 import { useSocialToken } from "../hooks";
 
@@ -8,12 +8,14 @@ const LoginForm = () => {
   const { signUpKakaoHandler, signUpNaverHandler } = useSocialToken();
 
   return (
-    <ButtonGroup>
-      <Box onClick={() => signUpKakaoHandler()}>
-        <KaKaoButtonIcon />
-      </Box>
-      <Button onClick={() => signUpNaverHandler()}>네이버 로그인</Button>
-    </ButtonGroup>
+    <Flex as={"form"}>
+      <ButtonGroup>
+        <Box onClick={() => signUpKakaoHandler()}>
+          <KaKaoButtonIcon />
+        </Box>
+        <Button onClick={() => signUpNaverHandler()}>네이버 로그인</Button>
+      </ButtonGroup>
+    </Flex>
   );
 };
 

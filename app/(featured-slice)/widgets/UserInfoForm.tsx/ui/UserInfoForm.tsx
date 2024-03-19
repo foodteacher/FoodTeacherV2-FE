650fe8760bbafbfb6b83b0aea84e170c39f32f95
527coco <527coco@naver.com>
2024-03-19 21:23:49 +0900

    feat : use hook form 의존성 추가
    Refs:[#9,#11]

--- app/(featured-slice)/widgets/UserInfoForm.tsx/ui/UserInfoForm.tsx
+++ app/(featured-slice)/widgets/UserInfoForm.tsx/ui/UserInfoForm.tsx
@@ -1,23 +1,68 @@
 "use client";
 
 import SttItem from "@/app/(featured-slice)/features/stt/ui/SttFormItem";
-import { Flex } from "@chakra-ui/react";
+import { Button } from "@/app/(featured-slice)/shared/UI";
+import { ButtonGroup, Flex, FormErrorMessage, VStack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { UserInfo } from "../types";
 
 const UserInfoForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<UserInfo>();
+
+  const onSubmit: SubmitHandler<UserInfo> = (formData) => {
+    console.log(formData);
+  };
+
   return (
-    <Flex as={"form"}>
-      <SttItem>
+    <VStack
+      as={"form"}
+      w={"80%"}
+      margin={"0 auto"}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <SttItem isInvalid={!!errors.name}>
         <Flex>
-          <SttItem.Label>123</SttItem.Label>
+          <SttItem.Label htmlFor="name">이름</SttItem.Label>
           <SttItem.SttButton />
         </Flex>
-        <SttItem.sttInput />
+        <SttItem.sttInput
+          id="name"
+          placeholder="이름을 입력해주세요."
+          register={register("name", {
+            required: "필수 입력 값입니다!",
+          })}
+        />
+        <FormErrorMessage>
+          {errors.name && errors.name.message}
+        </FormErrorMessage>
       </SttItem>
-      <SttItem>
-        <SttItem.SttButton />
-        <SttItem.sttInput />
+
+      <SttItem isInvalid={!!errors.gender}>
+        <Flex>
+          <SttItem.Label htmlFor="gender">성별</SttItem.Label>
+          <SttItem.SttButton />
+        </Flex>
+        <SttItem.sttInput
+          id="gender"
+          placeholder="성별을 입력해주세요."
+          register={register("gender", {
+            required: "필수 입력 값입니다!",
+          })}
+        />
+        <FormErrorMessage>
+          {errors.gender && errors.gender.message}
+        </FormErrorMessage>
       </SttItem>
-    </Flex>
+      <ButtonGroup>
+        <Button>뒤로가기</Button>
+        <Button type="submit">등록하기</Button>
+      </ButtonGroup>
+    </VStack>
   );
 };
 

26ba7e3417c8a8489f62fcf555d796fdcb6376ac
527coco <527coco@naver.com>
2024-03-21 20:41:14 +0900

    chore :코드 포맷팅 & Type 파일 정리
    Refs:[#2]

--- app/(featured-slice)/features/stt/types.ts
+++ app/(featured-slice)/features/stt/types.ts
@@ -1,6 +1,11 @@
-import { InputProps } from "@chakra-ui/react";
+import { FormControlProps, InputProps } from "@chakra-ui/react";
+import { FieldErrors } from "react-hook-form";
 
 /**userInfo */
+
+export interface FormItem extends FormControlProps {
+  errors: FieldErrors;
+}
 
 export interface SttInputProps extends InputProps {
   register?: any;

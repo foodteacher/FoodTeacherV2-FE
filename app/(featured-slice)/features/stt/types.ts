727f929a5c9e7427f5b2c31c178942914e98d78c
527coco <527coco@naver.com>
2024-03-21 19:39:41 +0900

    fix : UserInfo type 레이어 위치 변경
    Refs:[#1]

--- app/(featured-slice)/features/stt/types.ts
+++ app/(featured-slice)/features/stt/types.ts
@@ -1,9 +1,6 @@
 import { InputProps } from "@chakra-ui/react";
 
 /**userInfo */
-export interface UserInfo {
-  name: string;
-}
 
 export interface SttInputProps extends InputProps {
   register?: any;

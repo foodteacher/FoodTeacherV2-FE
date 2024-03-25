ae6bc852104ecdc503630e05586fbe6a6a2ab4a1
527coco <527coco@naver.com>
2024-03-25 22:18:58 +0900

    feat(proxy) : rewrites를 통해 cookie 받아오기
    Refs:[#2]

--- app/(featured-slice)/features/auth/api/api.ts
+++ app/(featured-slice)/features/auth/api/api.ts
@@ -1,30 +1,29 @@
 import { instacne } from "@/app/(featured-slice)/shared/api/SharedApi";
+import { Code } from "@/app/(featured-slice)/shared/type";
 import { redirect } from "next/navigation";
-
-type Code = { code: string; state?: string } | { code: string };
 
 export const getUser = async () => {
   const res = await instacne.get("/me");
   return res.data;
 };
 
-export const postKakaoCode = async (code: Code) => {
-  try {
-    const res = await instacne.post(`/login/kakao`, code);
+// export const postKakaoCode = async (code: Code) => {
+//   try {
+//     const res = await instacne.post(`/login/kakao`, code);
 
-    console.log(res);
-    const refresh = res.headers;
-    const access = res.data;
+//     console.log(res);
+//     const refresh = res.headers;
+//     const access = res.data;
 
-    return { access, refresh };
-  } catch (err) {
-    // return redirect("/");
-  }
-};
+//     return { access, refresh };
+//   } catch (err) {
+//     // return redirect("/");
+//   }
+// };
 
 export const postKakaoLogin = async (code: Code) => {
   try {
-    const res = await instacne.post(`/login/kakao`, { code });
+    const res = await instacne.post(`/login/kakao`, code);
     return res.data;
   } catch {
     return redirect("/");
@@ -34,7 +33,6 @@ export const postKakaoLogin = async (code: Code) => {
 export const postNaverLogin = async (code: Code) => {
   try {
     const res = await instacne.post("/login/naver", { code });
-    console.log(res);
     return res.data;
   } catch {
     return redirect("/");
--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 const nextConfig = {
+  async rewrites() {
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/:path*",
+          destination: `${BASE_URL}/:path*`,
+        },
+      ];
+    } else return [];
+  },
+
   experimental: {
     serverActions: true,
   },

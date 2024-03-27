27aa9a29184c01070dd0caf1de0c21a70833539f
527coco <527coco@naver.com>
2024-03-27 14:03:35 +0900

    feat : 로그인 실패 시 toast보여주기
    Refs:[#2]

--- app/(featured-slice)/features/auth/api/api.ts
+++ app/(featured-slice)/features/auth/api/api.ts
@@ -1,42 +1,27 @@
 import { instacne } from "@/app/(featured-slice)/shared/api/SharedApi";
 import { Code } from "@/app/(featured-slice)/shared/type";
-import { useRouter } from "next/navigation";
 
 export const getUser = async () => {
   const res = await instacne.get("/me");
   return res.data;
 };
 
-// export const postKakaoCode = async (code: Code) => {
-//   try {
-//     const res = await instacne.post(`/login/kakao`, code);
-
-//     const refresh = res.headers;
-//     const access = res.data;
-
-//     return { access, refresh };
-//   } catch (err) {
-//     // return redirect("/");
-//   }
-// };
-
 export const postKakaoLogin = async (code: Code) => {
-  const router = useRouter();
   try {
     const res = await instacne.post(`/login/kakao`, code);
     return res.data;
-  } catch {
-    return router.replace("/");
+  } catch (err) {
+    return null;
   }
 };
 
 export const postNaverLogin = async (code: Code) => {
-  const router = useRouter();
+  // const router = useRouter();
 
   try {
     const res = await instacne.post("/login/naver", { code });
     return res.data;
-  } catch {
-    return router.replace("/");
+  } catch (err) {
+    return null;
   }
 };

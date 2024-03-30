a27fc711180f69b2b360414c94893abc31a1d1df
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-03-30 17:06:47 +0900

    feat : useFunnel훅 최소 기능 정의
    Refs:[#26]

--- app/(featured-slice)/widgets/funnel/ui/funnel/Funnel.tsx
+++ app/(featured-slice)/widgets/funnel/ui/funnel/Funnel.tsx
@@ -1,13 +1,27 @@
 "use client";
-
 import React from "react";
 import { AgeStep, GenderStep } from "../step";
+import { useFunnel } from "../../hooks";
+import { Button } from "@/app/(featured-slice)/shared/UI";
 
 const Funnel = () => {
+  const { currentStep, onChangeNextStep, onChangePrevStep } = useFunnel([
+    "age",
+    "gender",
+  ]);
+
   return (
     <>
       <AgeStep />
       <GenderStep />
+      <Button
+        onClick={() => {
+          onChangeNextStep();
+          console.log(currentStep);
+        }}
+      >
+        go Next Step{" "}
+      </Button>
     </>
   );
 };

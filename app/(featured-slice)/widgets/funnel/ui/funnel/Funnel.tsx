e96febc2de2435bb487a2256b548036780802d20
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-03-30 15:49:29 +0900

    chore : funnel & step 초기 포맷 세팅
    Refs:[#26]

---
+++ app/(featured-slice)/widgets/funnel/ui/funnel/Funnel.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import { AgeStep, GenderStep } from "../step";
+
+const Funnel = () => {
+  return (
+    <>
+      <AgeStep />
+      <GenderStep />
+    </>
+  );
+};
+
+export default Funnel;

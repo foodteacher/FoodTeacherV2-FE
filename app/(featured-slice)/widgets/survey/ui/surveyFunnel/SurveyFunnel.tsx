9246a6ddfa6a1845f96ec98dffc851404e03324c
527coco <527coco@naver.com>
2024-04-01 18:23:07 +0900

    feat : accesstoken 재발급 로직
    Refs:[#2]

--- app/(featured-slice)/widgets/survey/ui/surveyFunnel/SurveyFunnel.tsx
+++ app/(featured-slice)/widgets/survey/ui/surveyFunnel/SurveyFunnel.tsx
@@ -4,6 +4,8 @@ import React, { useEffect } from "react";
 import { AgeStep } from "../step";
 import { useFunnel } from "@/app/(featured-slice)/shared/UI/Funnel/hook";
 import { useUser } from "@/app/(featured-slice)/shared/hooks";
+import { Button } from "@/app/(featured-slice)/shared/UI";
+import { updateAccessToken } from "@/app/(featured-slice)/shared/api/SharedApi";
 
 const SurveyFunnel = () => {
   const { onChangeNextStep, onChangePrevStep, currentStep } = useFunnel([
@@ -11,14 +13,16 @@ const SurveyFunnel = () => {
     "gender",
   ]);
 
-  const { userData } = useUser();
-
-  console.log(userData);
+  const refreshAccess = async () => {
+    const data = await updateAccessToken();
+    console.log(data);
+  };
 
   return (
     <Funnel>
       <Funnel.Step name="age">
         <AgeStep />
+        <Button onClick={refreshAccess}>refresh</Button>
       </Funnel.Step>
     </Funnel>
   );

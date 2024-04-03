2ad8c1f41bf78912f9ae6d1dc7e3927aad481669
527coco <527coco@naver.com>
2024-04-03 13:49:03 +0900

    Revert "feat : token type"
    This reverts commit 71116bfb09e5092718b5627af00cfd8076479593.

--- app/(featured-slice)/shared/type.ts
+++ app/(featured-slice)/shared/type.ts
@@ -6,10 +6,3 @@ export interface PageParams<T, U> {
 
 /**소셜로그인 코드 */
 export type Code = { code: string; state?: string; error: string };
-
-/**token */
-export interface Token {
-  accessToken: string;
-  refreshToken?: string;
-  tokenType?: string;
-}

eebd425d741ce73a22f46a754c4e302bf87b250d
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-04-06 16:54:16 +0900

    docs : 파일및 폴더 경로 대소문자 구분
    Refs:[#26]

--- app/(featured-slice)/shared/UI/Input/Input.tsx
+++
@@ -1,8 +0,0 @@
-import { InputProps } from "@chakra-ui/react";
-import { Input as ChakraInput } from "@chakra-ui/react";
-
-const Input = ({ ...props }: InputProps) => {
-  return <ChakraInput {...props} />;
-};
-
-export default Input;

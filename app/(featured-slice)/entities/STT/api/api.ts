0d57b5ecfc20c6247cae1b96b2a98be90681d21b
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-05-01 16:55:26 +0900

    feat : 서버 speechfile blob 변환
    Refs:[#50]

--- app/(featured-slice)/entities/STT/api/api.ts
+++ app/(featured-slice)/entities/STT/api/api.ts
@@ -1,6 +1,19 @@
 import { instacne } from "@/app/(featured-slice)/shared/api/SharedApi";
+import axios from "axios";
 
 export const postSpeechToText = async (text: string) => {
-  const res = await instacne.post("/", text);
+  const res = await axios.get("https://api2.foodteacher.xyz/voice/tts", {
+    responseType: "blob",
+    params: {
+      text,
+    },
+  });
+
+  const href = URL.createObjectURL(res.data);
+  console.log(href);
+  const audio = new Audio();
+  audio.src = href;
+  audio.play();
+
   return res.data;
 };

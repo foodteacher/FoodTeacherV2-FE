056d9da6bf07eb2912fe08beb555db1a8b768d92
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-05-15 17:30:02 +0900

    feat : step progress 버튼
    스텝 진행 프로그레스에 맞춰 디자인 변경
    
    Refs:[#21]

--- app/(featured-slice)/widgets/signupFunnel/types.ts
+++ app/(featured-slice)/widgets/signupFunnel/types.ts
@@ -4,19 +4,25 @@ import { Dispatch, SetStateAction } from "react";
 export interface StepProps {
   goNextStep: () => void;
   goPrevStep?: () => void;
-  setState: Dispatch<SetStateAction<SurveyState>>;
+  setState: Dispatch<SetStateAction<SignupState>>;
 }
 
-export interface SurveyState {
-  age: number;
-  gender: string;
+export type BloodType = "A" | "B" | "AB" | "O";
+export interface SignupState {
+  name: string;
+  height: number;
   weight: number;
+  birthday: string;
+  gender: string;
+  targetWeight: number;
+  bloodType: BloodType;
 }
 
-export type FunnelInfo = SurveyState;
+export type FunnelInfo = SignupState;
 
 export type FunnelState = {
   currentStep: string;
   funnelState: FunnelInfo;
   progress?: number;
+  steps?: string[];
 };
